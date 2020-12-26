307a0cc298cc201ce42b5188b54a951003ef3f6e
Felix Meissner <felix@Felixs-MBP.domain_not_set.invalid>
2020-12-26 19:44:14 +0100

    #26 lesson: React Router

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ab
+        obcaecati reiciendis aliquid illum cupiditate natus, consequatur
+        reprehenderit maxime molestiae voluptate in sapiente modi sunt ad
+        officia minus debitis id.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ab
+        obcaecati reiciendis aliquid illum cupiditate natus, consequatur
+        reprehenderit maxime molestiae voluptate in sapiente modi sunt ad
+        officia minus debitis id.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
